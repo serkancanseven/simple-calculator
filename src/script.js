5f34ca4fa0f33acc6392d830e0e5913ff9b70257
Serkan Canseven <serkancanseven@outlook.com>
2023-11-23 15:54:58 +0300

    make calculator functional

--- src/script.js
+++ src/script.js
@@ -1,5 +1,76 @@
-let display = 0;
-let firstNumber = 0;
-let secondNumber = 0;
+let displayValue = "0";
+let currentInput = "";
+let currentOperator = "";
 
-function printDisplay(display) {}
+const buttons = document.getElementsByClassName("button");
+for (const button of buttons) {
+  button.addEventListener("click", function () {
+    const buttonText = this.innerText;
+    if (!isNaN(parseInt(buttonText))) {
+      appendNumber(buttonText);
+    } else if (buttonText === "C") {
+      clearDisplay();
+    } else {
+      operate(buttonText);
+    }
+  });
+}
+
+function clearDisplay() {
+  displayValue = "0";
+  currentInput = "";
+  currentOperator = "";
+  updateDisplay();
+}
+
+function updateDisplay() {
+  document.getElementById("display").innerText = displayValue;
+}
+
+function appendNumber(number) {
+  if (displayValue === "0") {
+    displayValue = number;
+  } else {
+    displayValue += number;
+  }
+  updateDisplay();
+}
+
+function operate(operator) {
+  if (currentInput !== "") {
+    calculateResult();
+  }
+  currentOperator = operator;
+  currentInput = displayValue;
+  displayValue = "0";
+}
+
+function calculateResult() {
+  if (currentInput !== "" && currentOperator !== "") {
+    switch (currentOperator) {
+      case "÷":
+        displayValue = (
+          parseInt(currentInput) / parseInt(displayValue)
+        ).toString();
+        break;
+      case "×":
+        displayValue = (
+          parseInt(currentInput) * parseInt(displayValue)
+        ).toString();
+        break;
+      case "–":
+        displayValue = (
+          parseInt(currentInput) - parseInt(displayValue)
+        ).toString();
+        break;
+      case "+":
+        displayValue = (
+          parseInt(currentInput) + parseInt(displayValue)
+        ).toString();
+        break;
+    }
+    currentInput = "";
+    currentOperator = "";
+    updateDisplay();
+  }
+}
